88385b7ce78de2f3121ba8b35187dff9b6e1beb0
Ashish Rai <ashish.rai@appinventiv.com>
2025-08-14 16:08:57 +0530

    Wrap contact page with Scaffold and simplify resume page implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:pdfx/pdfx.dart';
-import 'package:provider/provider.dart';
 import 'app.dart';
 
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
-  runApp(MultiProvider(
-    providers: [
-      // Provider(create: (context) => ContactProvider(sendMessageUseCase: SendMessageUseCase(ContactRepositoryImpl(ContactDataSourceImpl())))),
-    ],
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
-class ResumeSection extends StatelessWidget {
-  const ResumeSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            'My Resume',
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
-        ),
-        Expanded(
-          child: Container(
-            margin: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey.shade300),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: PdfView(
-              controller: PdfController(
-                document: PdfDocument.openAsset('assets/cv.pdf'),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
